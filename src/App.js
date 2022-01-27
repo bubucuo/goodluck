768747269c489de9b56583f5e882ebe8a0af6fb4
bubucuo <276185876@qq.com>
2022-01-27 11:41:16 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from "react";
+
 export default function App(props) {
+  const [name, setName] = useState("");
+
+  const getLuck = () => {};
+
   return (
-    <div>
-      <h3>App</h3>
+    <div className="app">
+      <div className="name-box">
+        <input
+          className="name"
+          type="text"
+          value={name}
+          placeholder="请输入姓名"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <button onClick={getLuck}>查看运势</button>
+      </div>
+
+      <div className="canvas">
+        <img src={"../public/imgs/fu.jpeg"} />
+      </div>
     </div>
   );
 }
