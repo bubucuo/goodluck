980fcf5b480c17ea34fc77aded0f951b0ae3d4e4
bubucuo <276185876@qq.com>
2022-01-30 17:46:24 +0800

    update main

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import classnames from "classnames";
+import { getGoodLuck, getLuckNum } from "./utils";
+
+const defaultState = {
+  // loading为true代表铜钱在旋转
+  loading: false,
+  // 长度为5、默认值为0的数组，（0代表硬币反面，1代表正面）
+  data: new Array(5).fill(0),
+  luck: {
+    // 算命的结果语
+    destiny: "",
+    // 一个class数组，标记不同命运的不同动画效果，具体的代码定义在index.css
+    classNames: [],
+    // 头图，如金钱豹
+    banner: "",
+  },
+};
+
+export default function App(props) {
+  const [wish, setWish] = useState("");
+
+  const [state, setState] = useState(defaultState);
+  const getLuck = () => {
+    let newState = { ...defaultState };
+    if (state.loading) {
+      newState.loading = false;
+      // 获取五枚铜钱的正反面，玄机查看utils
+      for (let i = 0; i < 5; i++) {
+        newState.data[i] = getLuckNum();
+      }
+      newState.luck = getGoodLuck(wish);
+    } else {
+      newState.loading = true;
+    }
+    setState(newState);
+  };
+
+  const reset = () => {
+    setState(defaultState);
+  };
+
+  return (
+    <div className="app">
+      <a
+        className="like"
+        href="https://juejin.cn/post/7058915357561454629"
+        target="_blank"
+      >
+        不准不要赞
+      </a>
+
+      <div className="center">
+        <input
+          className="wish"
+          type="text"
+          value={wish}
+          placeholder="说出你的愿望吧"
+          onChange={(e) => {
+            setWish(e.target.value);
+          }}
+          onKeyUp={(e) => {
+            if (e.keyCode === 13) {
+              getLuck();
+            }
+          }}
+        />
+        <button onClick={getLuck}>
+          {state.loading ? "查看结果" : "查看运势"}
+        </button>
+      </div>
+
+      <div className="canvas">
+        {state.data.map((item, index) => (
+          <div
+            key={index}
+            className={classnames(state.loading && "coin-rotate", "coin")}
+          >
+            <div
+              className={classnames(
+                "coin0",
+                !state.loading && item === 1 && "hide"
+              )}
+            ></div>
+            <div
+              className={classnames(
+                "coin1",
+                !state.loading && item === 0 && "hide"
+              )}
+            ></div>
+          </div>
+        ))}
+      </div>
+
+      {state.luck?.classNames != "" && !state.loading && (
+        <div className={classnames("luck-bg center", state.luck?.mainStyle)}>
+          {state.luck?.classNames &&
+            state.luck?.classNames.map((item, index) => (
+              <div
+                key={index}
+                className={classnames("luck", item, "center")}
+              ></div>
+            ))}
+
+          <div
+            className={classnames(state.luck.banner, "banner", "center")}
+          ></div>
+          <div className="blessing center">{state.luck?.destiny}</div>
+          <button className="reset" onClick={reset}>
+            一键重启
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/utils.js
@@ -0,0 +1,97 @@
+// 获取五枚铜钱的正反面，当然是随机数，你想什么呢，难不成还看你生辰八字
+export function getLuckNum() {
+  return Math.round(Math.random());
+}
+
+function isEmpty(wish) {
+  const numReg = /^[0-9]+.?[0-9]*$/;
+  if (numReg.test(wish)) {
+    return true;
+  }
+  wish = wish.replaceAll(" ", "");
+  return wish === "" || wish === "null" || wish === "undefined";
+}
+
+// wish: string, keys: Array
+function match(wish, keys) {
+  for (let i = 0; i < keys.length; i++) {
+    if (wish.indexOf(keys[i]) > -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const defaultLuck = {
+  destiny: "哇，新的一年，是你的人生巅峰哎，居然事事顺心如意，还能暴富！！！",
+  classNames: ["gold-coins", "gold-coins2"],
+  banner: "leopard-all",
+};
+
+export function getGoodLuck(wish) {
+  wish += "";
+  if (isEmpty(wish)) {
+    return {
+      ...defaultLuck,
+      destiny: "🤩许愿都这么懒，那就祝你明年在法律允许的范围内能不劳而获啦！",
+    };
+  }
+  let res = { destiny: "", classNames: [], mainStyle: "", banner: "" };
+  for (let i = 0; i < lucks.length; i++) {
+    const { keys, destiny, classNames, mainStyle = "", banner = "" } = lucks[i];
+    if (match(wish, keys)) {
+      res.destiny += destiny;
+      res.classNames = [...res.classNames, ...classNames];
+      res.mainStyle = mainStyle;
+      res.banner = banner;
+    }
+  }
+
+  return res.classNames.length > 0 ? res : defaultLuck;
+}
+
+const lucks = [
+  {
+    keys: ["脱单", "女朋友", "男朋友", "对象"],
+    destiny:
+      "🤩新的一年，快主动点，你的桃花正在加速度靠近你，千万不要再躲开了！",
+    classNames: ["hua-bg"],
+    mainStyle: "main-pink",
+    banner: "taohuayun",
+  },
+  {
+    keys: ["富", "钱", "薪", "升职"],
+    destiny: "💰新的一年，财神选中你了，赶紧想想买哪里的豪宅吧！",
+    classNames: ["gold-coins"],
+    banner: "leopard-money",
+  },
+  {
+    keys: ["美", "漂亮", "好看", "仙"],
+    destiny:
+      "👧🏻生气，你都已经这么美了，还要美下去，仙女都要嫉妒了！新的一年，你还是好好健身吧，毕竟都这么美了",
+    classNames: ["gold-coins"],
+    banner: "leopard-all",
+  },
+  {
+    keys: ["帅"],
+    destiny:
+      "👦🏻生气，吴彦祖都没你帅，你居然还不满足！新的一年，你还是好好健身吧，毕竟都这么帅了！",
+    classNames: ["gold-coins"],
+    banner: "leopard-all",
+  },
+
+  {
+    keys: ["吃", "锅", "肉"],
+    destiny:
+      "🍚重庆火锅？东北烤肉？北京烤鸭？广东烧鹅？成都串串？河南胡辣汤？全部安排！！！",
+    classNames: ["food"],
+    banner: "eat",
+  },
+
+  {
+    keys: ["减肥", "健", "胖", "头发", "瘦"],
+    destiny: "💪🏻健康最重要，新的一年，好好健身，每天坚持才有效果哦！！！",
+    classNames: ["gold-coins"],
+    banner: "leopard-all",
+  },
+];
